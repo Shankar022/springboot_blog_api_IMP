d5d4e84bf184a13e8b37bed5861028305bbcb3d9
Shankar Chakraborty <40225075+Shankar022@users.noreply.github.com>
2023-09-09 18:52:40 +0530

    finished post entity

---
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -0,0 +1,29 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+// This default constructor(NoArgsConstructor) is used by JPA to create instances of the entity class using Reflection.
+// It provides a way to dynamically create instances of classes, call methods, and access fields.
+@Entity
+@Table( name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
