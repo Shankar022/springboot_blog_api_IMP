5e03e2aa0cc6149812eaf49ce7281e8c9cdb76b0
Shankar Chakraborty <40225075+Shankar022@users.noreply.github.com>
2023-09-10 17:41:45 +0530

    Update PostService.java

--- src/main/java/com/springboot/blog/service/PostService.java
+++ src/main/java/com/springboot/blog/service/PostService.java
@@ -6,15 +6,22 @@ import com.springboot.blog.payload.PostResponse;
 import java.util.List;
 
 public interface PostService {
+
+    // Create a post
     PostDto createPost(PostDto postDto);
 
+    // Create multiple posts
     List<PostDto> createPosts(List<PostDto> postDtoList);
 
+    // Get all posts with pagination and sorting
     PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
 
+    // Get a post by its ID
     PostDto getPostById(long id);
 
+    // Update a post by its ID
     PostDto updatePost(PostDto postDto, long id);
 
+    // Delete a post by its ID
     void deletePost(long id);
 }
