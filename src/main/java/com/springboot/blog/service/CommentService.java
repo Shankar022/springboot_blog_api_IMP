88d7bebf0253863567ecff334d2eac05cc9a6532
Shankar Chakraborty <40225075+Shankar022@users.noreply.github.com>
2023-09-10 17:41:04 +0530

    Update CommentService.java

--- src/main/java/com/springboot/blog/service/CommentService.java
+++ src/main/java/com/springboot/blog/service/CommentService.java
@@ -5,13 +5,19 @@ import com.springboot.blog.payload.CommentDto;
 import java.util.List;
 
 public interface CommentService {
+
+    // Create a comment for a post
     CommentDto createComment(long postId, CommentDto commentDto);
 
+    // Get all comments for a post by post ID
     List<CommentDto> getCommentsByPostId(long postId);
 
+    // Get a comment by its ID
     CommentDto getCommentById(long postId, long commentId);
 
+    // Update a comment by its ID
     CommentDto updateComment(long postId, long commentId, CommentDto commentDto);
 
+    // Delete a comment by its ID
     void deleteComment(long postId, long commentId);
 }
