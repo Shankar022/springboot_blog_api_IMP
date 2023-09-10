58290d90e87f01062273585e4f0c106c5e6c6d77
Shankar Chakraborty <40225075+Shankar022@users.noreply.github.com>
2023-09-10 17:32:41 +0530

    Update CommentServiceImpl.java

--- src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -26,48 +26,66 @@ public class CommentServiceImpl implements CommentService {
         this.postRepository = postRepository;
     }
 
+    // Create a new comment for a specific post
     @Override
     public CommentDto createComment(long postId, CommentDto commentDto) {
         Comment newComment = mapDtoToEntity(commentDto);
+
+        // Find the associated post or throw a ResourceNotFoundException
         Post post = postRepository.findById(postId).orElseThrow(
                 () -> new ResourceNotFoundException("Post", "Id", postId));
 
+        // Set the post for the new comment and save it
         newComment.setPost(post);
         Comment comment = commentRepository.save(newComment);
+
+        // Map Entity to DTO and return it
         return mapEntityToDto(comment);
     }
 
+    // Get all comments for a specific post
     @Override
     public List<CommentDto> getCommentsByPostId(long postId) {
+        // Find all comments associated with the post
         List<Comment> commentList = commentRepository.findCommentsByPostId(postId);
+
+        // Map each comment Entity to DTO and return as a list
         return commentList.stream().map(this::mapEntityToDto).toList();
     }
 
+    // Get a specific comment by its ID, ensuring it belongs to the specified post
     @Override
     public CommentDto getCommentById(long postId, long commentId) {
         Comment comment = validateCommentBelongsToPost(postId, commentId);
+
+        // Map Entity to DTO and return it
         return mapEntityToDto(comment);
     }
 
+    // Update a specific comment by its ID, ensuring it belongs to the specified post
     @Override
     public CommentDto updateComment(long postId, long commentId, CommentDto commentDto) {
         Comment comment = validateCommentBelongsToPost(postId, commentId);
 
+        // Map DTO to Entity for the updated comment properties
         Comment requestComment = mapDtoToEntity(commentDto);
         comment.setName(requestComment.getName());
         comment.setEmail(requestComment.getEmail());
         comment.setBody(requestComment.getBody());
+
+        // Save the updated comment and return it
         Comment updatedComment = commentRepository.save(comment);
         return mapEntityToDto(updatedComment);
     }
 
+    // Delete a specific comment by its ID, ensuring it belongs to the specified post
     @Override
     public void deleteComment(long postId, long commentId) {
         Comment comment = validateCommentBelongsToPost(postId, commentId);
 
+        // Delete the comment
         commentRepository.delete(comment);
     }
-
 
     // Define a method to validate if a comment belongs to a post
     private Comment validateCommentBelongsToPost(long postId, long commentId) {
@@ -87,8 +105,7 @@ public class CommentServiceImpl implements CommentService {
         return comment;
     }
 
-
-    // convert DTO to Entity function
+    // Convert DTO to Entity function
     private Comment mapDtoToEntity(CommentDto commentRequestDto) {
         Comment comment = new Comment();
         comment.setId(commentRequestDto.getId());
@@ -98,7 +115,7 @@ public class CommentServiceImpl implements CommentService {
         return comment;
     }
 
-    // convert Entity to DTO function
+    // Convert Entity to DTO function
     private CommentDto mapEntityToDto(Comment newComment) {
         return new CommentDto(
                 newComment.getId(),
