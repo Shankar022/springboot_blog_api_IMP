2f9c35e3c53878dbb78b381e70b11033b60129db
Shankar Chakraborty <40225075+Shankar022@users.noreply.github.com>
2023-09-10 17:30:41 +0530

    add delete update comment endpoint

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -23,19 +23,48 @@ public class CommentController {
     @PostMapping("/{postId}/comments")
     public ResponseEntity<CommentDto> createComment(
             @PathVariable(value = "postId") long postId,
-            @RequestBody CommentDto commentRequestDto){
-            CommentDto commentResponseDto = commentService.createComment(postId, commentRequestDto);
-            return new ResponseEntity<>(commentResponseDto, HttpStatus.CREATED);
+            @RequestBody CommentDto commentRequestDto) {
+        CommentDto commentResponseDto = commentService.createComment(postId, commentRequestDto);
+        return new ResponseEntity<>(commentResponseDto, HttpStatus.CREATED);
     }
 
     // get all comments by a post id
     @GetMapping("/{postId}/comments")
-    public ResponseEntity<List<CommentDto>> getCommentsByPostId(@PathVariable(value = "postId") long postId){
+    public ResponseEntity<List<CommentDto>> getCommentsByPostId(@PathVariable(value = "postId") long postId) {
         List<CommentDto> commentDtoList = commentService.getCommentsByPostId(postId);
         return new ResponseEntity<>(commentDtoList, HttpStatus.OK);
     }
-            
 
+    // get comment by id
+    @GetMapping("/{postId}/comments/{commentId}")
+    public ResponseEntity<CommentDto> getCommentById(
+            @PathVariable(value = "postId") long postId,
+            @PathVariable(value = "commentId") long commentId
+    ) {
+        CommentDto commentResponseDto = commentService.getCommentById(postId, commentId);
+        return new ResponseEntity<>(commentResponseDto, HttpStatus.OK);
+    }
+
+    // update a comment by id
+    @PutMapping("/{postId}/comments/{commentId}")
+    public ResponseEntity<CommentDto> updateComment(
+            @PathVariable(value = "postId") long postId,
+            @PathVariable(value = "commentId") long commentId,
+            @RequestBody CommentDto commentRequestDto
+    ) {
+        CommentDto commentResponseDto = commentService.updateComment(postId, commentId, commentRequestDto);
+        return new ResponseEntity<>(commentResponseDto, HttpStatus.OK);
+    }
+
+    // delete a comment
+    @DeleteMapping("/{postId}/comments/{commentId}")
+    public ResponseEntity<String> updateComment(
+            @PathVariable(value = "postId") long postId,
+            @PathVariable(value = "commentId") long commentId
+    ) {
+        commentService.deleteComment(postId, commentId);
+        return new ResponseEntity<>(String.format("Comment deleted successfully, ID : %s ", commentId), HttpStatus.OK);
+    }
 
 
 }
--- src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -2,12 +2,14 @@ package com.springboot.blog.service.impl;
 
 import com.springboot.blog.entity.Comment;
 import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.BlogAPIException;
 import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.payload.CommentDto;
 import com.springboot.blog.repository.CommentRepository;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -39,6 +41,50 @@ public class CommentServiceImpl implements CommentService {
     public List<CommentDto> getCommentsByPostId(long postId) {
         List<Comment> commentList = commentRepository.findCommentsByPostId(postId);
         return commentList.stream().map(this::mapEntityToDto).toList();
+    }
+
+    @Override
+    public CommentDto getCommentById(long postId, long commentId) {
+        Comment comment = validateCommentBelongsToPost(postId, commentId);
+        return mapEntityToDto(comment);
+    }
+
+    @Override
+    public CommentDto updateComment(long postId, long commentId, CommentDto commentDto) {
+        Comment comment = validateCommentBelongsToPost(postId, commentId);
+
+        Comment requestComment = mapDtoToEntity(commentDto);
+        comment.setName(requestComment.getName());
+        comment.setEmail(requestComment.getEmail());
+        comment.setBody(requestComment.getBody());
+        Comment updatedComment = commentRepository.save(comment);
+        return mapEntityToDto(updatedComment);
+    }
+
+    @Override
+    public void deleteComment(long postId, long commentId) {
+        Comment comment = validateCommentBelongsToPost(postId, commentId);
+
+        commentRepository.delete(comment);
+    }
+
+
+    // Define a method to validate if a comment belongs to a post
+    private Comment validateCommentBelongsToPost(long postId, long commentId) {
+        // Find the associated post or throw a ResourceNotFoundException
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new ResourceNotFoundException("Post", "Id", postId));
+
+        // Find the comment by its ID or throw a ResourceNotFoundException
+        Comment comment = commentRepository.findById(commentId).orElseThrow(
+                () -> new ResourceNotFoundException("Comment", "Id", commentId));
+
+        // Check if the comment belongs to the specified post, otherwise throw a BlogAPIException
+        if (!comment.getPost().getId().equals(post.getId())) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Comment doesn't belong to the post !");
+        }
+
+        return comment;
     }
 
 
